b415c9f7802daefceae40c85408ff10471414a24
Hasan Veldstra <hasan.veldstra@gmail.com>
2012-10-09 12:48:40 +0100

    Rewrite tab switching with jQuery

--- js/musichipster.js
+++ js/musichipster.js
@@ -2,15 +2,14 @@ window.onload = function() {
 
     var tabs = function() {
         var args = models.application.arguments;
-        var current = document.getElementById(args[0]);
-        var sections = document.getElementsByClassName("section");
-        for(i = 0; i < sections.length; i++) {
-            sections[i].style.display = "none";
-        }
-        current.style.display = "block";
+        var current = $("#"+args[0]);
+        var sections = $(".section");
+        sections.hide();
+        current.show();
     }
 
     sp = getSpotifyApi(1);
     var models = sp.require("sp://import/scripts/api/models");
     models.application.observe(models.EVENT.ARGUMENTSCHANGED, tabs);
 }
+
