5c85d8f88760eab11f13ccd50ae40687c5ff665c
Hasan Veldstra <hasan.veldstra@gmail.com>
2012-10-09 17:39:21 +0100

    Calculating score 1 - albums and artists

--- js/musichipster.js
+++ js/musichipster.js
@@ -1,5 +1,7 @@
-window.onload = function() {
+var sp = getSpotifyApi(1);
+var models = models = sp.require("sp://import/scripts/api/models");
 
+window.onload = function() {
     var tabs = function() {
         var args = models.application.arguments;
         var current = $("#"+args[0]);
@@ -8,12 +10,20 @@ window.onload = function() {
         current.show();
     }
 
-    sp = getSpotifyApi(1);
-    var models = sp.require("sp://import/scripts/api/models");
     models.application.observe(models.EVENT.ARGUMENTSCHANGED, tabs);
 
     $("#judgeMeButton").click(function() {
-        console.info("judging...");
+        var score = calculateScore();
+        $("#score").html(score).show();
     });
 }
 
+var calculateScore = function() {
+    // 1 point for each artist & album -- encourage variety, variety is good
+    // 10 extra points for each hip artist -- encourage hip music, hip music is good
+    // -100 points for each lame artist -- strongly discourage Bieber etc
+    // -500 points if there's a top track by a lame artist
+    var albums = models.library.albums;
+    var artists = models.library.artists;
+    return albums.length + artists.length;
+}
